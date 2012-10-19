d5bcb0d54f4344fdd6258af8a6ce8c3522c31d14
Tim Kuntz <tkuntz@groupon.com>
2012-10-19 12:45:19 -0500

    Return query results as CSV instead of XML

--- lib/salesforce_bulk_api/job.rb
+++ lib/salesforce_bulk_api/job.rb
@@ -20,14 +20,14 @@ module SalesforceBulkApi
       if !@@external_field.nil? # This only happens on upsert
         xml += "<externalIdFieldName>#{@@external_field}</externalIdFieldName>"
       end
-      xml += "<contentType>XML</contentType>"
+      xml += "<contentType>CSV</contentType>"
       xml += "</jobInfo>"
 
       path = "job"
       headers = Hash['Content-Type' => 'application/xml; charset=utf-8']
 
       response = @@connection.post_xml(nil, path, xml, headers)
-      response_parsed = XmlSimple.xml_in(response)    
+      response_parsed = XmlSimple.xml_in(response)
       puts response
       @@job_id = response_parsed['id'][0]
     end
@@ -48,8 +48,8 @@ module SalesforceBulkApi
 
     def add_query
       path = "job/#{@@job_id}/batch/"
-      headers = Hash["Content-Type" => "application/xml; charset=UTF-8"]
-      
+      headers = Hash["Content-Type" => "text/csv; charset=UTF-8"]
+
       response = @@connection.post_xml(nil, path, @@records, headers)
       response_parsed = XmlSimple.xml_in(response)
 
@@ -65,7 +65,7 @@ module SalesforceBulkApi
         super_records<<@@records_dup.pop(10000)
       end
       super_records<<@@records_dup
-      
+
       super_records.each do|batch|
         xml = "#{@@XML_HEADER}<sObjects xmlns=\"http://www.force.com/2009/06/asyncapi/dataload\">"
         batch.each do |r|
@@ -76,13 +76,13 @@ module SalesforceBulkApi
           xml += "</sObject>"
         end
         xml += "</sObjects>"
-    
+
 
         path = "job/#{@@job_id}/batch/"
         headers = Hash["Content-Type" => "application/xml; charset=UTF-8"]
         response = @@connection.post_xml(nil, path, xml, headers)
         response_parsed = XmlSimple.xml_in(response)
-      
+
         @@batch_id = response_parsed['id'][0]
         puts @@batch_id
       end
